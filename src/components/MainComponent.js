e636c7bbb6b14104261401e9cecd0e39ab5eb2ba
Kassem Sleiman <kassem.sleiman@hotmail.com>
2022-04-20 17:30:15 +0300

    About

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Header from './HeaderComponent';
 import Home from './HomeComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -51,6 +52,7 @@ class Main extends Component {
         <Header />
         <Switch>
               <Route path='/home' component={HomePage} />
+              <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />
